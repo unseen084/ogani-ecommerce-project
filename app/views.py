f4d62e63a61e002c9b82d31de0f242454e31d950
unseen084 <mushfiqur696@gmail.com>
2022-03-08 16:34:38 +0600

    blog objects shown from models

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 
 def home(request):
-    return render(request, 'app/index.html')
+    blogs = Blog.objects.all()
+    return render(request, 'app/index.html', {'blogs': blogs})
