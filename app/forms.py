ff29e9eb385b3f65af0de70860bc6fc98d881f95
DESKTOP-83JQO3M\User <xyyy2967@gmail.com>
2022-03-14 21:50:55 +0600

    [User Profile showing(partial)]

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
@@ -31,6 +32,39 @@ class CustomerRegistrationForm(UserCreationForm):
         labels = {'email': 'Email'}
         widgets = {'username': forms.TextInput(attrs={'class': 'form-control'})}
         fields = ['username', 'first_name', 'last_name', 'country', 'address', 'city', 'postal_code', 'phone_number', 'password1', 'password2', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+
+    first_name = forms.CharField(label='First Name', max_length=12, min_length=4, required=False,
+                                 help_text='Required: First Name',
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label='Last Name', max_length=12, min_length=4, required=False,
+                                help_text='Required: Last Name',
+                                widget=(forms.TextInput(attrs={'class': 'form-control'})))
+    country = forms.CharField(label='Country', required=False, help_text='Required: Country',
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}))
+    address = forms.CharField(label='Address', required=False, help_text='Required: Address',
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}))
+    city = forms.CharField(label='City', required=False, help_text='Required: City',
+                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}))
+    postal_code = forms.CharField(label='Postal Code', required=False, help_text='Required: Zip Code',
+                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zip Code'}))
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = forms.CharField(validators=[phone_regex], max_length=17,
+                                   required=False)  # validators should be a list
+    email = forms.CharField(required=False, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'country', 'address', 'city', 'postal_code', 'phone_number', 'email']
+
+    #class Meta:
+    #    model = User
+    #    labels = {'email': 'Email'}
+    #    widgets = {'username': forms.TextInput(attrs={'class': 'form-control'})}
+    #    fields = ['username', 'first_name', 'last_name', 'country', 'address', 'city', 'postal_code', 'phone_number', 'password1', 'password2', 'email']
 
 
 class LoginForm(AuthenticationForm):
